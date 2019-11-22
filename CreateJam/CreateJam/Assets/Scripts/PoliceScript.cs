1875c6fc831133ef95a26f78a07cfb9185b2be2f
jthoms19 <jthoms19@student.aau.dk>
2019-11-22 23:35:40 +0100

    Backup jeg virker!

---
+++ CreateJam/CreateJam/Assets/Scripts/PoliceScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceScript : MonoBehaviour
+{
+    public GameObject Enemy;
+    public GameObject Player;
+    public Transform Target;
+    public float AggroRange;
+    private float range;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        range = Vector2.Distance(Enemy.transform.position, Player.transform.position);
+    }
+}
